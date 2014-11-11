7b94a8943d0374f48f5a46571455a08fcfd32a3f
rorygreig <rorygreig@gmail.com>
2014-11-11 15:35:20 +0000

    use array concat rather than append

--- sendEmails.js
+++ sendEmails.js
@@ -44,6 +44,9 @@ var excludedEmails = [
   'oskar.paolini@gmail.com',
   'marzell.camenzind@gmail.com',
   'carl@centrabit.com',
+  'spry@sdf.org',
+  'teppo.salonen@gmail.com',
+  'github@mathisonian.com'
 ];
 
 console.log(contacts.length);
@@ -55,28 +58,37 @@ var emailFuncs = Array();
 
 contacts.forEach(function(contact){
   if(excludedEmails.indexOf(contact.email) == -1){
-    console.log(contact.email);
-    console.log(contact.name);
-    console.log(contact.bitcoinrepo);
+    if(contact.email !== undefined && contact.email !== null && contact.email !== ""){
+      console.log('\n');
+      console.log(contact.email);
+      console.log(contact.name);
+      console.log(contact.bitcoinrepo);
 
-    emailFuncs.push(function(callback){
-      email.sendFollowUpEmail(contact.email, contact.name, contact.bitcoinrepo, function(response){
-        console.log(response);
-        if(response.indexOf("250") > -1){
-          sentEmails.push(contact.email);
-          console.log("Sent email to: " + contact.email);
-          counter++;
-          callback();
-        }
-      });
-    });
+      emailFuncs.push(function(callback){
+          console.log('\n');
+          console.log(contact.email);
+
+          email.sendFollowUpEmail(contact.email, contact.name, contact.bitcoinrepo, function(response){
+            console.log(response);
+            if(response.indexOf("250") > -1){
+              sentEmails.push(contact.email);
+              console.log("Sent email to: " + contact.email);
+              counter++;
+              callback();
+            }
+          });
+        });
+
+    } else {
+      console.log("email undefined");
+    }
   }
 });
 
 async.series(emailFuncs, function(){
   console.log(sentEmails);
   console.log("Sent " + sentEmails.length + " emails");
-  excldudedEmails = excludedEmails.append(sentEmails);
+  excludedEmails = excludedEmails.concat(sentEmails);
   saveToJSON(excludedEmails);
 });
 
